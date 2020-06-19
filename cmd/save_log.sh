176a788051ec36cc0ca765977b0343c822023e4b
CastielLiu <castiel_liu@outlook.com>
2020-06-19 15:30:22 +0800

    ..

--- cmd/save_log.sh
+++ cmd/save_log.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 #roslaunch recorder record_drive_status.launch file_path:=$1
-roslaunch recorder record_gps_ins_node.launch file_path:=$1
+roslaunch recorder record_gps_ins.launch file_path:=$1
 
--- cmd/stop_driverless.sh
+++ cmd/stop_driverless.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 rosnode kill /driverless_node 
-rosnode kill /record_drive_status_node
+rosnode kill /record_gps_ins_node
 
